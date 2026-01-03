23cd1219161a4d73d29fc6930f7d9adcc6e1e92b
NivaPatel10 <nivapatel102006@gmail.com>
2026-01-03 15:27:45 +0530

    Added all the pages with the integration using django

--- Mysite/Mysite/views.py
+++ Mysite/Mysite/views.py
@@ -3,3 +3,11 @@ def home(request):
     return render(request,'home.html')
 def login(request):
     return render(request,"login.html")
+def dashboard(request):
+    return render(request,"dashboard.html")
+def signup(request):
+    return render(request,"signup.html")
+def login_view(request):
+    return render(request,"login.html")
+def profile(request):
+    return render(request,"profile.html")
